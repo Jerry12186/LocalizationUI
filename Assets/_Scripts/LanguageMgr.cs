58847ee76983f95ddd034817d49650212b199c15
Jerry12186 <Jerry12186@163.com>
2018-05-11 16:07:05 +0800

    Signed-off-by: Jerry12186 <Jerry12186@163.com>

---
+++ Assets/_Scripts/LanguageMgr.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+public class LanguageMgr
+{
+    private static LanguageMgr _instance;
+    private LanguageMgr() { }
+
+    private SystemLanguage language;
+
+    public static LanguageMgr GetInstance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new LanguageMgr();
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// 设置语言
+    /// </summary>
+    /// <param name="lang"></param>
+    public void SetLanguage(SystemLanguage lang)
+    {
+        //第一次使用的话，根据系统语言进行设置
+        if (!PlayerPrefs.HasKey("Language"))
+        {
+            PlayerPrefs.SetInt("Language", (int)GetCurrentLangue());
+        }
+        else
+        {
+            PlayerPrefs.SetString("Language", lang.ToString());
+        }
+        ///更新UI
+        ///TODO
+    }
+
+    public void SetLanguage()
+    {
+        SetLanguage(GetCurrentLangue());
+    }
+
+    /// <summary>
+    /// 获取系统当前的语言
+    /// </summary>
+    public SystemLanguage GetCurrentLangue()
+    {
+        return Application.systemLanguage;
+    }
+
+    /// <summary>
+    /// 获取语言库
+    /// </summary>
+    /// <returns></returns>
+    public string GetLanguageData(SystemLanguage lang)
+    {
+        return Resources.Load(lang.ToString()).ToString();
+    }
+    /// <summary>
+    /// 获取语言库，默认时调用
+    /// </summary>
+    /// <returns></returns>
+    public string GetLanguageData()
+    {
+        return Resources.Load(GetCurrentLangue().ToString()).ToString();
+    }
+}
+
+#region 用于Json
+[System.Serializable]
+public class TextsOfUI
+{
+    public WeaponPanel WeaponPanel = new WeaponPanel();
+}
+[System.Serializable]
+public class WeaponPanel : WeapPanelParent
+{
+    public string WeaponIcon_Text;
+    public string WeaponDiscrible_Name;
+    public string WeaponDiscrible_Property_Property01;
+    public string WeaponDiscrible_Property_Property01_Number;
+    public string WeaponDiscrible_Property_Property02;
+    public string WeaponDiscrible_Property_Property02_Number;
+    public string WeaponDiscrible_Property_Property03;
+    public string WeaponDiscrible_Property_Property03_Number;
+    public string WeaponDiscrible_Property_Property04;
+    public string WeaponDiscrible_Property_Property04_Number;
+    public string WeaponDiscrible_Property_Property05;
+    public string WeaponDiscrible_Property_Property05_Number;
+    public string WeaponDiscrible_Property_Discrible;
+}
+
+public class WeapPanelParent
+{
+    public virtual string Text { get; set; }
+}
+#endregion
---
+++ Assets/_Scripts/ReflectTools.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class ReflectTools : MonoBehaviour
+{
+    /// <summary>
+    /// 动态创建一个类
+    /// </summary>
+    /// <param name="className"></param>
+    public static object CreateInstacne(string className)
+    {
+        Type type = Type.GetType(className);
+        if (type != null)
+            return Activator.CreateInstance(type);
+        return null;
+    }
+
+    /// <summary>
+    /// 从另外一个类复制字段到另外一个类
+    /// </summary>
+    /// <param name="to">要改变字段的类</param>
+    /// <param name="from">来源类</param>
+    public static void SetFieldFromOtherClass(object to, object from)
+    {
+        int toCount = to.GetType().GetFields().Length;
+        int fromCount = from.GetType().GetFields().Length;
+
+        if (toCount != fromCount)
+        {
+            throw new Exception("目标类和来源类的字段个数不一致");
+        }
+        else
+        {
+            for (int i = 0; i < toCount; i++)
+            {
+                string fieldName = GetFieldsFromClass(to)[i].Name;
+                SetFieldVaule(to, fieldName, from.GetType().GetField(fieldName).GetValue(from).ToString());
+            }
+        }
+    }
+
+    /// <summary>
+    /// 获取某个字段
+    /// </summary>
+    /// <param name="myClass"></param>
+    /// <param name="fieldName"></param>
+    /// <returns></returns>
+    public static FieldInfo[] GetFieldsFromClass(object myClass)
+    {
+        return myClass.GetType().GetFields();
+    }
+
+    /// <summary>
+    /// 获取类中字段的值
+    /// </summary>
+    /// <param name="myClass"></param>
+    /// <param name="fieldName"></param>
+    /// <returns></returns>
+    public static object GetFieldFromClass(object myClass, string fieldName)
+    {
+        return myClass.GetType().GetField(fieldName).GetValue(myClass);
+    }
+    /// <summary>
+    /// 从fromClass获取字段赋值到toClass对应的字段上
+    /// </summary>
+    /// <param name="toClass"></param>
+    /// <param name="fromClass"></param>
+    /// <param name="toValue"></param>
+    /// <param name="fromValue"></param>
+    private static void SetFieldVaule(object toClass, string toValue,string fromValue)
+    {
+        toClass.GetType().GetField(toValue).SetValue(toClass, fromValue);
+    }
+}
